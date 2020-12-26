18418a9cca02a1bea1f6957f37c8a9ba86fe602a
lenkdn <71015688+lenkdn@users.noreply.github.com>
2020-12-26 14:18:27 +0300

    Add files via upload

---
+++ Lab10/Matrix.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+
+namespace Lab10
+{
+    class Matrix
+    {
+        private int[,] matrix;
+        int m, n;
+
+        public Matrix() { }
+
+        public void GenerateMatrix(int M, int N)
+        {
+            m = M; n = N;
+
+            Random r = new Random();
+
+            matrix = new int[M, N];
+
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    matrix[i, j] = r.Next(1000);
+                }
+            }
+        }
+
+        public void SaveMatrix(string pFileName)
+        {
+            if (matrix.Length > 0)
+            {
+                if (File.Exists(pFileName))
+                    File.Delete(pFileName);
+
+                FileInfo f = new FileInfo(pFileName);
+                TextWriter tw = f.CreateText();
+
+                tw.WriteLine(m.ToString());
+                tw.WriteLine(n.ToString());
+
+                for (int i = 0; i < m; i++)
+                    for (int j = 0; j < n; j++)
+                        tw.WriteLine(i.ToString() + " " + j.ToString() + " " + matrix[i, j].ToString());
+
+                tw.Close();
+            }
+        }
+
+        public Boolean LoadMatrix(string pFileName)
+        {
+            if (File.Exists(pFileName))
+            {
+                try
+                {
+                    TextReader tr = File.OpenText(pFileName);
+                    m = Convert.ToInt32(tr.ReadLine());
+                    n = Convert.ToInt32(tr.ReadLine());
+
+                    matrix = new int[m, n];
+                    string line;
+                    string[] substring;
+
+                    for (int i = 0; i < m; i++)
+                        for (int j = 0; j < n; j++)
+                        {
+                            line = tr.ReadLine();
+                            substring = line.Split(new char[] { ' ' }, 3);
+                            matrix[i, j] = Convert.ToInt32(substring[2]);
+                        }
+
+                    tr.Close();
+
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+
+            return false;
+        }
+
+        public void PrintMatrix()
+        {
+            if (matrix.Length > 0)
+            {
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                        Console.Write(matrix[i, j].ToString() + " ");
+
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("\n");
+            }
+        }
+
+    }
+}
---
+++ Lab10/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Sum_diag_elem_ch_stol_in_matrix(string pFileName)
+            {
+                int sum = 0;
+                if (File.Exists(pFileName))
+                {
+                    try
+                    {
+                        string line;
+                        string[] substring;
+
+                        TextReader tr = File.OpenText(pFileName);
+                        int m = Convert.ToInt32(tr.ReadLine());
+                        int n = Convert.ToInt32(tr.ReadLine());
+                        int perem = 0;
+                        if (n % 2 == 0)
+                            perem = n + 1;
+
+                        for (int i = 0; i < m; i++)
+                            for (int j = 0; j < n; j++)
+                            {
+                                line = tr.ReadLine();
+                                substring = line.Split(new char[] { ' ' }, 3);
+                                if ((j % 2 != 0) && ((i == j) | ((i + j) == (perem - 1))))
+                                    sum += Convert.ToInt32(substring[2]);
+                            }
+
+                        tr.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else
+                    throw new FileNotFoundException();
+
+                return sum;
+            }
+
+            Matrix m1 = new Matrix();
+            Matrix m2 = new Matrix();
+
+            m1.GenerateMatrix(4, 4);
+            m1.SaveMatrix("Matrix_1.txt");
+
+            m2.GenerateMatrix(3, 3);
+            m2.SaveMatrix("Matrix_2.txt");
+
+            if (m1.LoadMatrix("Matrix_1.txt") && m2.LoadMatrix("Matrix_2.txt"))
+            {
+                Console.WriteLine("Матрица 1\n");
+                m1.PrintMatrix();
+                Console.WriteLine("Матрица 2\n");
+                m2.PrintMatrix();
+                Console.WriteLine("Сумма диагональных элементов\n" +
+                    "чётных столбцов матриц: " + (Sum_diag_elem_ch_stol_in_matrix("Matrix_1.txt") +
+                    Sum_diag_elem_ch_stol_in_matrix("Matrix_2.txt")));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
