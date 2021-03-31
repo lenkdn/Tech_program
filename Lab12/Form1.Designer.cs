940a57a080e0f2e3d2b2e6880677f0a9fa07fd9b
lenkdn <71015688+lenkdn@users.noreply.github.com>
2021-03-31 10:55:08 +0300

    Add files via upload

---
+++ Lab12/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Lab12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_x = new System.Windows.Forms.TextBox();
+            this.label_x = new System.Windows.Forms.Label();
+            this.label_z = new System.Windows.Forms.Label();
+            this.textBox_ax1 = new System.Windows.Forms.TextBox();
+            this.label_ax1 = new System.Windows.Forms.Label();
+            this.textBox_z = new System.Windows.Forms.TextBox();
+            this.label_y = new System.Windows.Forms.Label();
+            this.textBox_y = new System.Windows.Forms.TextBox();
+            this.label_e7 = new System.Windows.Forms.Label();
+            this.textBox_e7 = new System.Windows.Forms.TextBox();
+            this.label_as_9 = new System.Windows.Forms.Label();
+            this.textBox_as_9 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_x
+            // 
+            this.textBox_x.Location = new System.Drawing.Point(118, 105);
+            this.textBox_x.Name = "textBox_x";
+            this.textBox_x.ReadOnly = true;
+            this.textBox_x.Size = new System.Drawing.Size(100, 22);
+            this.textBox_x.TabIndex = 0;
+            this.textBox_x.TabStop = false;
+            this.textBox_x.Text = "0";
+            // 
+            // label_x
+            // 
+            this.label_x.AutoSize = true;
+            this.label_x.Location = new System.Drawing.Point(98, 108);
+            this.label_x.Name = "label_x";
+            this.label_x.Size = new System.Drawing.Size(14, 17);
+            this.label_x.TabIndex = 1;
+            this.label_x.Text = "x";
+            // 
+            // label_z
+            // 
+            this.label_z.AutoSize = true;
+            this.label_z.Location = new System.Drawing.Point(324, 108);
+            this.label_z.Name = "label_z";
+            this.label_z.Size = new System.Drawing.Size(15, 17);
+            this.label_z.TabIndex = 3;
+            this.label_z.Text = "z";
+            // 
+            // textBox_ax1
+            // 
+            this.textBox_ax1.Location = new System.Drawing.Point(345, 153);
+            this.textBox_ax1.Name = "textBox_ax1";
+            this.textBox_ax1.Size = new System.Drawing.Size(100, 22);
+            this.textBox_ax1.TabIndex = 1;
+            this.textBox_ax1.Text = "0";
+            // 
+            // label_ax1
+            // 
+            this.label_ax1.AutoSize = true;
+            this.label_ax1.Location = new System.Drawing.Point(309, 156);
+            this.label_ax1.Name = "label_ax1";
+            this.label_ax1.Size = new System.Drawing.Size(30, 17);
+            this.label_ax1.TabIndex = 5;
+            this.label_ax1.Text = "ax1";
+            // 
+            // textBox_z
+            // 
+            this.textBox_z.Location = new System.Drawing.Point(345, 105);
+            this.textBox_z.Name = "textBox_z";
+            this.textBox_z.Size = new System.Drawing.Size(100, 22);
+            this.textBox_z.TabIndex = 0;
+            this.textBox_z.Text = "0";
+            // 
+            // label_y
+            // 
+            this.label_y.AutoSize = true;
+            this.label_y.Location = new System.Drawing.Point(98, 156);
+            this.label_y.Name = "label_y";
+            this.label_y.Size = new System.Drawing.Size(15, 17);
+            this.label_y.TabIndex = 7;
+            this.label_y.Text = "y";
+            // 
+            // textBox_y
+            // 
+            this.textBox_y.Location = new System.Drawing.Point(118, 153);
+            this.textBox_y.Name = "textBox_y";
+            this.textBox_y.ReadOnly = true;
+            this.textBox_y.Size = new System.Drawing.Size(100, 22);
+            this.textBox_y.TabIndex = 6;
+            this.textBox_y.TabStop = false;
+            this.textBox_y.Text = "0";
+            // 
+            // label_e7
+            // 
+            this.label_e7.AutoSize = true;
+            this.label_e7.Location = new System.Drawing.Point(315, 203);
+            this.label_e7.Name = "label_e7";
+            this.label_e7.Size = new System.Drawing.Size(24, 17);
+            this.label_e7.TabIndex = 9;
+            this.label_e7.Text = "e7";
+            // 
+            // textBox_e7
+            // 
+            this.textBox_e7.Location = new System.Drawing.Point(345, 200);
+            this.textBox_e7.Name = "textBox_e7";
+            this.textBox_e7.Size = new System.Drawing.Size(100, 22);
+            this.textBox_e7.TabIndex = 2;
+            this.textBox_e7.Text = "0";
+            // 
+            // label_as_9
+            // 
+            this.label_as_9.AutoSize = true;
+            this.label_as_9.Location = new System.Drawing.Point(300, 248);
+            this.label_as_9.Name = "label_as_9";
+            this.label_as_9.Size = new System.Drawing.Size(39, 17);
+            this.label_as_9.TabIndex = 11;
+            this.label_as_9.Text = "as_9";
+            // 
+            // textBox_as_9
+            // 
+            this.textBox_as_9.Location = new System.Drawing.Point(345, 245);
+            this.textBox_as_9.Name = "textBox_as_9";
+            this.textBox_as_9.Size = new System.Drawing.Size(100, 22);
+            this.textBox_as_9.TabIndex = 3;
+            this.textBox_as_9.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 17);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Координаты курсора мыши:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(334, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 17);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Ввод значений:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 332);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_as_9);
+            this.Controls.Add(this.textBox_as_9);
+            this.Controls.Add(this.label_e7);
+            this.Controls.Add(this.textBox_e7);
+            this.Controls.Add(this.label_y);
+            this.Controls.Add(this.textBox_y);
+            this.Controls.Add(this.label_ax1);
+            this.Controls.Add(this.textBox_z);
+            this.Controls.Add(this.label_z);
+            this.Controls.Add(this.textBox_ax1);
+            this.Controls.Add(this.label_x);
+            this.Controls.Add(this.textBox_x);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_x;
+        private System.Windows.Forms.Label label_x;
+        private System.Windows.Forms.Label label_z;
+        private System.Windows.Forms.TextBox textBox_ax1;
+        private System.Windows.Forms.Label label_ax1;
+        private System.Windows.Forms.TextBox textBox_z;
+        private System.Windows.Forms.Label label_y;
+        private System.Windows.Forms.TextBox textBox_y;
+        private System.Windows.Forms.Label label_e7;
+        private System.Windows.Forms.TextBox textBox_e7;
+        private System.Windows.Forms.Label label_as_9;
+        private System.Windows.Forms.TextBox textBox_as_9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Lab12/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lab12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            float x, y, z, ax1, e7, as_9, Z;
+            x = e.X;
+            textBox_x.Text = e.X.ToString();
+            y = e.Y;
+            textBox_y.Text = e.Y.ToString();
+
+            try
+            {
+                z = Convert.ToSingle(textBox_z.Text);
+                ax1 = Convert.ToSingle(textBox_ax1.Text);
+                e7 = Convert.ToSingle(textBox_e7.Text);
+                as_9 = Convert.ToSingle(textBox_as_9.Text);
+                Z = Convert.ToSingle(z * ax1 + Math.Sqrt(Math.Abs(e7 + x / y)) - Math.Abs(x + Math.Cos(as_9 + y)));
+                Text = "Z = " + Z.ToString();
+            }
+            catch (Exception)
+            {
+                Text = "ERROR";
+            }
+        }
+    }
+}
