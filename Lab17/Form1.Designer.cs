636430cc20866a0c424e7d748137db888f231a8d
lenkdn <71015688+lenkdn@users.noreply.github.com>
2021-05-19 10:01:16 +0300

    Add files via upload

---
+++ Lab17/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Lab17
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnChooseColor = new System.Windows.Forms.Button();
+            this.btnStopAnimation = new System.Windows.Forms.Button();
+            this.btnStartAnimation = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label_X = new System.Windows.Forms.Label();
+            this.label_Y = new System.Windows.Forms.Label();
+            this.label_0 = new System.Windows.Forms.Label();
+            this.label_2r = new System.Windows.Forms.Label();
+            this.label_2u = new System.Windows.Forms.Label();
+            this.label_2d = new System.Windows.Forms.Label();
+            this.label_2l = new System.Windows.Forms.Label();
+            this.label_1d = new System.Windows.Forms.Label();
+            this.label_1l = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnChooseColor
+            // 
+            this.btnChooseColor.Location = new System.Drawing.Point(519, 13);
+            this.btnChooseColor.Margin = new System.Windows.Forms.Padding(4);
+            this.btnChooseColor.Name = "btnChooseColor";
+            this.btnChooseColor.Size = new System.Drawing.Size(131, 69);
+            this.btnChooseColor.TabIndex = 1;
+            this.btnChooseColor.Text = "Выбор цвета закрашенной области";
+            this.btnChooseColor.UseVisualStyleBackColor = true;
+            this.btnChooseColor.Click += new System.EventHandler(this.BtnChooseColor_Click);
+            // 
+            // btnStopAnimation
+            // 
+            this.btnStopAnimation.Location = new System.Drawing.Point(519, 457);
+            this.btnStopAnimation.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStopAnimation.Name = "btnStopAnimation";
+            this.btnStopAnimation.Size = new System.Drawing.Size(131, 55);
+            this.btnStopAnimation.TabIndex = 2;
+            this.btnStopAnimation.Text = "Остановить анимацию";
+            this.btnStopAnimation.UseVisualStyleBackColor = true;
+            this.btnStopAnimation.Click += new System.EventHandler(this.btnStopAnimation_Click);
+            // 
+            // btnStartAnimation
+            // 
+            this.btnStartAnimation.Location = new System.Drawing.Point(519, 394);
+            this.btnStartAnimation.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStartAnimation.Name = "btnStartAnimation";
+            this.btnStartAnimation.Size = new System.Drawing.Size(131, 55);
+            this.btnStartAnimation.TabIndex = 3;
+            this.btnStartAnimation.Text = "Запустить анимацию";
+            this.btnStartAnimation.UseVisualStyleBackColor = true;
+            this.btnStartAnimation.Click += new System.EventHandler(this.btnStartAnimation_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox1_Paint);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseMove);
+            // 
+            // label_X
+            // 
+            this.label_X.AutoSize = true;
+            this.label_X.Location = new System.Drawing.Point(450, 250);
+            this.label_X.Name = "label_X";
+            this.label_X.Size = new System.Drawing.Size(17, 17);
+            this.label_X.TabIndex = 5;
+            this.label_X.Text = "X";
+            // 
+            // label_Y
+            // 
+            this.label_Y.AutoSize = true;
+            this.label_Y.Location = new System.Drawing.Point(250, 50);
+            this.label_Y.Name = "label_Y";
+            this.label_Y.Size = new System.Drawing.Size(17, 17);
+            this.label_Y.TabIndex = 6;
+            this.label_Y.Text = "Y";
+            // 
+            // label_0
+            // 
+            this.label_0.AutoSize = true;
+            this.label_0.Location = new System.Drawing.Point(245, 236);
+            this.label_0.Name = "label_0";
+            this.label_0.Size = new System.Drawing.Size(16, 17);
+            this.label_0.TabIndex = 7;
+            this.label_0.Text = "0";
+            // 
+            // label_2r
+            // 
+            this.label_2r.AutoSize = true;
+            this.label_2r.Location = new System.Drawing.Point(419, 269);
+            this.label_2r.Name = "label_2r";
+            this.label_2r.Size = new System.Drawing.Size(16, 17);
+            this.label_2r.TabIndex = 8;
+            this.label_2r.Text = "2";
+            // 
+            // label_2u
+            // 
+            this.label_2u.AutoSize = true;
+            this.label_2u.Location = new System.Drawing.Point(245, 119);
+            this.label_2u.Name = "label_2u";
+            this.label_2u.Size = new System.Drawing.Size(16, 17);
+            this.label_2u.TabIndex = 9;
+            this.label_2u.Text = "2";
+            // 
+            // label_2d
+            // 
+            this.label_2d.AutoSize = true;
+            this.label_2d.Location = new System.Drawing.Point(286, 394);
+            this.label_2d.Name = "label_2d";
+            this.label_2d.Size = new System.Drawing.Size(16, 17);
+            this.label_2d.TabIndex = 10;
+            this.label_2d.Text = "2";
+            // 
+            // label_2l
+            // 
+            this.label_2l.AutoSize = true;
+            this.label_2l.Location = new System.Drawing.Point(112, 269);
+            this.label_2l.Name = "label_2l";
+            this.label_2l.Size = new System.Drawing.Size(16, 17);
+            this.label_2l.TabIndex = 11;
+            this.label_2l.Text = "2";
+            // 
+            // label_1d
+            // 
+            this.label_1d.AutoSize = true;
+            this.label_1d.Location = new System.Drawing.Point(286, 328);
+            this.label_1d.Name = "label_1d";
+            this.label_1d.Size = new System.Drawing.Size(16, 17);
+            this.label_1d.TabIndex = 12;
+            this.label_1d.Text = "1";
+            // 
+            // label_1l
+            // 
+            this.label_1l.AutoSize = true;
+            this.label_1l.Location = new System.Drawing.Point(172, 236);
+            this.label_1l.Name = "label_1l";
+            this.label_1l.Size = new System.Drawing.Size(16, 17);
+            this.label_1l.TabIndex = 13;
+            this.label_1l.Text = "1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 522);
+            this.Controls.Add(this.label_1l);
+            this.Controls.Add(this.label_1d);
+            this.Controls.Add(this.label_2l);
+            this.Controls.Add(this.label_2d);
+            this.Controls.Add(this.label_2u);
+            this.Controls.Add(this.label_2r);
+            this.Controls.Add(this.label_0);
+            this.Controls.Add(this.label_Y);
+            this.Controls.Add(this.label_X);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnStartAnimation);
+            this.Controls.Add(this.btnStopAnimation);
+            this.Controls.Add(this.btnChooseColor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnChooseColor;
+        private System.Windows.Forms.Button btnStopAnimation;
+        private System.Windows.Forms.Button btnStartAnimation;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label_X;
+        private System.Windows.Forms.Label label_Y;
+        private System.Windows.Forms.Label label_0;
+        private System.Windows.Forms.Label label_2r;
+        private System.Windows.Forms.Label label_2u;
+        private System.Windows.Forms.Label label_2d;
+        private System.Windows.Forms.Label label_2l;
+        private System.Windows.Forms.Label label_1d;
+        private System.Windows.Forms.Label label_1l;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Lab17/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Lab17
+{
+    public partial class Form1 : Form
+    {
+        const int R1 = 50, R2 = 100;
+        int D1 = R1 * 2,
+            D2 = R2 * 2;
+        float centerX = 200;
+        float centerY = 200;
+
+        Brush br1 = new SolidBrush(DefaultBackColor);
+        Brush br2 = new SolidBrush(DefaultBackColor);
+        Brush new_br = new SolidBrush(Color.DarkGray);
+
+        Boolean DropStarted = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            foreach (Control ctrl in this.Controls)
+            {
+                if ((ctrl as Label) != null)
+                {
+                    (ctrl as Label).MouseDown += label_MouseDown;
+                    (ctrl as Label).MouseMove += label_MouseMove;
+                    (ctrl as Label).MouseUp += label_MouseUp;
+                }
+            }
+        }
+
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics figure = e.Graphics;
+            figure.DrawEllipse(Pens.Black, centerX - R1, centerY - R1, D1, D1);
+            figure.DrawEllipse(Pens.Black, centerX - R2, centerY - R2, D2, D2);
+            figure.DrawEllipse(Pens.Black, centerX - R2, centerY, D1, D1);
+
+            Pen lineX = new Pen(Color.Black, 2) { CustomEndCap = new AdjustableArrowCap(5, 5) },
+                lineY = new Pen(Color.Black, 2) { CustomEndCap = new AdjustableArrowCap(5, 5) },
+                lineDiag = new Pen(Color.Black, 2),
+                lineStrih = new Pen(Color.Black, 2);
+
+            figure.DrawLine(lineX, 50, centerY, pictureBox1.Width - 50, centerY);
+            figure.DrawLine(lineY, centerX, pictureBox1.Height - 50, centerX, 50);
+            figure.DrawLine(lineDiag, centerX - R2, centerY + R2, centerX + R2 + 10, centerY - R2 - 10);
+
+            lineStrih.DashStyle = DashStyle.Dash;
+            figure.DrawLine(lineStrih, centerX, centerY - R2, centerX + R2, centerY - R2);
+            figure.DrawLine(lineStrih, centerX + R2, centerY, centerX + R2, centerY - R2);
+
+            figure.FillEllipse(Brushes.Black, centerX - R1 - 5, centerY - 5, 10, 10);
+            figure.FillEllipse(Brushes.Black, centerX - 5, centerY + R1 - 5, 10, 10);
+            figure.FillEllipse(Brushes.Black, centerX + R2 - 5, centerY - 5, 10, 10);
+            figure.FillEllipse(Brushes.Black, centerX + R2 - 5, centerY - R2 - 5, 10, 10);
+
+            GraphicsPath gp1 = new GraphicsPath();
+            gp1.AddPie(centerX - R2, centerY - R2, D2, D2, 270, 45);
+            gp1.AddPie(centerX - R1, centerY - R1, D1, D1, 270, 45);
+            Region r1 = new Region(gp1);
+
+            GraphicsPath gp2 = new GraphicsPath();
+            gp2.AddPie(centerX - R2, centerY, D1, D1, -45, 180);
+            GraphicsPath gp3 = new GraphicsPath();
+            gp3.AddPie(centerX - R2, centerY - R2, D2, D2, 90, 45);
+            gp3.AddEllipse(centerX - R1, centerY - R1, D1, D1);
+            Region r2 = new Region(gp2);
+            Region r3 = new Region(gp3);
+            r2.Intersect(r3);
+            figure.FillRegion(br1, r1);
+            figure.FillRegion(br2, r2);
+        }
+
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int X = e.X, Y = e.Y;
+            Brush old = new SolidBrush(DefaultBackColor);
+
+            GraphicsPath gp1 = new GraphicsPath();
+            gp1.AddPie(centerX - R2, centerY - R2, D2, D2, 270, 45);
+            gp1.AddPie(centerX - R1, centerY - R1, D1, D1, 270, 45);
+            Region r1 = new Region(gp1);
+
+            GraphicsPath gp2 = new GraphicsPath();
+            gp2.AddPie(centerX - R2, centerY, D1, D1, -45, 180);
+            GraphicsPath gp3 = new GraphicsPath();
+            gp3.AddPie(centerX - R2, centerY - R2, D2, D2, 90, 45);
+            gp3.AddEllipse(centerX - R1, centerY - R1, D1, D1);
+            Region r2 = new Region(gp2);
+            Region r3 = new Region(gp3);
+            r2.Intersect(r3);
+
+            if (r1.IsVisible(X, Y))
+                br1 = new_br;
+            else
+            {
+                br1 = old;
+                pictureBox1.Invalidate();
+            }
+
+            if (r2.IsVisible(X, Y))
+                br2 = new_br;
+            else
+            {
+                br2 = old;
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void BtnChooseColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog dlg = new ColorDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                new_br = new SolidBrush(dlg.Color);
+            }
+        }
+
+        private void label_MouseDown(object sender, MouseEventArgs e)
+        {
+            DropStarted = true;
+        }
+
+        private void label_MouseMove(object sender, MouseEventArgs e)
+        {
+            Control c = sender as Control;
+            if (DropStarted)
+            {
+                c.Location = this.PointToClient(Control.MousePosition);
+            }
+        }
+
+        private void label_MouseUp(object sender, MouseEventArgs e)
+        {
+            DropStarted = false;
+        }
+
+        private void btnStartAnimation_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void btnStopAnimation_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            Color new_color = Color.FromArgb(r.Next(256), r.Next(256), r.Next(256));
+            int new_size = r.Next(10, 15);
+
+            foreach (Label label in this.Controls.OfType<Label>())
+            {
+                label.ForeColor = new_color;
+                label.Font = new Font(Font.FontFamily, new_size);
+            }
+        }
+    }
+}
